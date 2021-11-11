648cbcfd5e99d85df90b197af214f964f0757362
Jack Crompton <Jcrompton57@gmail.com>
2021-11-11 10:38:38 +0000

    Chage city to 'Albury' Change Scatter plot to red dots

--- R/create-plot.R
+++ R/create-plot.R
@@ -9,13 +9,15 @@ df = read_csv("data/weatherAUS.csv")
 # Create a scatter plot of Humidity9am and Temp9am
 # a location
 
-city = "Brisbane"
+city = "Albury"
 
 scatter_plot = 
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point()
+  geom_point(color = 'red')
+
+
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
